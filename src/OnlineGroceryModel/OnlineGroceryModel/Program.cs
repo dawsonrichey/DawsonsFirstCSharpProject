20c269d4bd573675e2d1bdf9890904684ef726d7
Dawson Richey <dawsonrichey@gmail.com>
2018-06-19 00:28:17 -0400

    Online Grocery app initalized

---
+++ src/OnlineGroceryModel/OnlineGroceryModel/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineGroceryModel.Models;
+
+namespace OnlineGroceryModel
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var context = new Context())
+			{
+				context.OnlineGroceries.Add(new Models.OnlineGrocery()
+				{
+					FoodName = "Bananna",
+					FoodCost = 2.99,
+					PublishedOn = DateTime.Today
+				});
+				context.SaveChanges();
+
+				var onlineGroceries = context.OnlineGroceries.ToList();
+				foreach (var onlineGrocery in onlineGroceries)
+				{
+					Console.WriteLine(onlineGrocery.FoodName);
+				}
+				Console.ReadLine();
+
+			}
+
+		}
+	}
+}
